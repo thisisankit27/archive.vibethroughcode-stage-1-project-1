e989f7fd80dca1a466f24ed1b14c8c496cb51d63
thisisankit27 <thisisankitsrivastava@gmail.com>
2026-07-23 14:38:07 +0530

    PR-5: Vector Store

--- data/src/ingestion.py
+++ data/src/ingestion.py
@@ -1,7 +1,9 @@
 from data.src.factory_pattern.document_loader_service import load_documents
 from data.src.strategy_pattern.chunk_service import chunk_documents
 from data.src.embeddings import EmbeddingService
+from data.src.vector_store import VectorStore
 
+vector_store = VectorStore()
 
 def ingest_documents(uploaded_files):
 
@@ -10,5 +12,7 @@ def ingest_documents(uploaded_files):
     chunks = chunk_documents(documents)
 
     embeddings, dimension, elapsed = EmbeddingService.generate_embeddings(chunks)
+
+    vector_store.store(chunks, embeddings)
 
     return chunks, embeddings, dimension, elapsed
